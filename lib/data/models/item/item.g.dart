1bf40dcf40360098a927e46422a78c312172e18b
josipbarisic <josip.barisic1998@gmail.com>
2024-04-12 14:46:02 +0200

    Add: freezed-generated data model files

---
+++ lib/data/models/item/item.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ItemImpl _$$ItemImplFromJson(Map<String, dynamic> json) => _$ItemImpl(
+      productID: json['productID'] as String,
+      quantity: json['quantity'] as int,
+    );
+
+Map<String, dynamic> _$$ItemImplToJson(_$ItemImpl instance) =>
+    <String, dynamic>{
+      'productID': instance.productID,
+      'quantity': instance.quantity,
+    };
